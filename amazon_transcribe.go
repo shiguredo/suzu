7836195ce2f3622cf3ee4921431fe7869e9b9100
Yoshida Hiroshi <yoshida@shiguredo.jp>
2023-02-10 17:02:32 +0900

    gcp と同様の処理内容に整理する

--- amazon_transcribe.go
+++ amazon_transcribe.go
@@ -2,7 +2,6 @@ package suzu
 
 import (
 	"context"
-	"fmt"
 	"io"
 	"net/http"
 
@@ -12,18 +11,16 @@ import (
 )
 
 type AmazonTranscribe struct {
-	LanguageCode                        string
-	MediaEncoding                       string
-	MediaSampleRateHertz                int64
-	EnablePartialResultsStabilization   bool
-	NumberOfChannels                    int64
-	EnableChannelIdentification         bool
-	PartialResultsStability             string
-	Region                              string
-	Debug                               bool
-	StartStreamTranscriptionEventStream *transcribestreamingservice.StartStreamTranscriptionEventStream
-	ResultCh                            chan AwsResult
-	Config                              Config
+	LanguageCode                      string
+	MediaEncoding                     string
+	MediaSampleRateHertz              int64
+	EnablePartialResultsStabilization bool
+	NumberOfChannels                  int64
+	EnableChannelIdentification       bool
+	PartialResultsStability           string
+	Region                            string
+	Debug                             bool
+	Config                            Config
 }
 
 func NewAmazonTranscribe(config Config, languageCode string, sampleRateHertz, audioChannelCount int64) *AmazonTranscribe {
@@ -36,7 +33,6 @@ func NewAmazonTranscribe(config Config, languageCode string, sampleRateHertz, au
 		PartialResultsStability:           config.AwsPartialResultsStability,
 		NumberOfChannels:                  audioChannelCount,
 		EnableChannelIdentification:       config.AwsEnableChannelIdentification,
-		ResultCh:                          make(chan AwsResult),
 		Config:                            config,
 	}
 }
@@ -92,41 +88,26 @@ func NewAmazonTranscribeClient(config Config) *transcribestreamingservice.Transc
 	return transcribestreamingservice.New(sess, cfg)
 }
 
-func (at *AmazonTranscribe) Start(ctx context.Context, config Config, r io.Reader) error {
-	if err := at.startTranscribeService(ctx, config); err != nil {
-		return err
-	}
-
-	if err := at.streamAudioFromReader(ctx, r, FrameSize); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func (at *AmazonTranscribe) startTranscribeService(ctx context.Context, config Config) error {
-
+func (at *AmazonTranscribe) Start(ctx context.Context, config Config, r io.Reader) (*transcribestreamingservice.StartStreamTranscriptionEventStream, error) {
 	client := NewAmazonTranscribeClient(config)
 	input := NewStartStreamTranscriptionInput(at)
 
 	resp, err := client.StartStreamTranscriptionWithContext(ctx, &input)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	stream := resp.GetStream()
-	at.StartStreamTranscriptionEventStream = stream
 
-	go at.ReceiveResults(ctx)
+	go func() {
+		defer stream.Close()
 
-	return nil
-}
+		if err := transcribestreamingservice.StreamAudioFromReader(ctx, stream, FrameSize, r); err != nil {
+			return
+		}
+	}()
 
-func (at *AmazonTranscribe) Close() error {
-	if at.StartStreamTranscriptionEventStream != nil {
-		return at.StartStreamTranscriptionEventStream.Close()
-	}
-	return nil
+	return stream, nil
 }
 
 type AwsResult struct {
@@ -152,56 +133,4 @@ func (ar *AwsResult) WithChannelID(channelID string) *AwsResult {
 func (ar *AwsResult) WithIsPartial(isPartial bool) *AwsResult {
 	ar.IsPartial = &isPartial
 	return ar
-}
-
-func (at *AmazonTranscribe) ReceiveResults(ctx context.Context) {
-L:
-	for {
-		select {
-		case <-ctx.Done():
-			return
-		case event := <-at.StartStreamTranscriptionEventStream.Events():
-			switch e := event.(type) {
-			case *transcribestreamingservice.TranscriptEvent:
-				for _, res := range e.Transcript.Results {
-					var result AwsResult
-					result.Type = "aws"
-					if at.Config.AwsResultIsPartial {
-						result.WithIsPartial(*res.IsPartial)
-					}
-					if at.Config.AwsResultChannelID {
-						result.WithChannelID(*res.ChannelId)
-					}
-					for _, alt := range res.Alternatives {
-						var message string
-						if alt.Transcript != nil {
-							message = *alt.Transcript
-						}
-						result.Message = message
-
-						at.ResultCh <- result
-					}
-				}
-			default:
-				break L
-			}
-		}
-	}
-
-	if err := at.StartStreamTranscriptionEventStream.Err(); err != nil {
-		err := fmt.Errorf("UNEXPECTED-STREAM-EVENT: %w", err)
-		at.ResultCh <- AwsErrorResult(err)
-		return
-	}
-
-	// io.EOF の場合は err は nil になるため明示的に io.EOF を送る
-	at.ResultCh <- AwsErrorResult(io.EOF)
-
-}
-
-func (at *AmazonTranscribe) streamAudioFromReader(ctx context.Context, r io.Reader, frameSize int) error {
-	if err := transcribestreamingservice.StreamAudioFromReader(ctx, at.StartStreamTranscriptionEventStream, frameSize, r); err != nil {
-		return err
-	}
-	return nil
 }
--- amazon_transcribe_handler.go
+++ amazon_transcribe_handler.go
@@ -3,7 +3,10 @@ package suzu
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"io"
+
+	"github.com/aws/aws-sdk-go/service/transcribestreamingservice"
 )
 
 func init() {
@@ -11,40 +14,66 @@ func init() {
 }
 
 func AmazonTranscribeHandler(ctx context.Context, reader io.Reader, args HandlerArgs) (*io.PipeReader, error) {
-	at := NewAmazonTranscribe(args.Config, args.LanguageCode, int64(args.SampleRate), int64(args.ChannelCount))
-
 	oggReader, oggWriter := io.Pipe()
 	go func() {
+		defer oggWriter.Close()
 		if err := opus2ogg(ctx, reader, oggWriter, args.SampleRate, args.ChannelCount, args.Config); err != nil {
 			oggWriter.CloseWithError(err)
 			return
 		}
-		oggWriter.Close()
 	}()
 
-	go func() {
-		defer at.Close()
-
-		if err := at.Start(ctx, args.Config, oggReader); err != nil {
-			at.ResultCh <- AwsErrorResult(err)
-			return
-		}
-	}()
+	at := NewAmazonTranscribe(args.Config, args.LanguageCode, int64(args.SampleRate), int64(args.ChannelCount))
+	stream, err := at.Start(ctx, args.Config, oggReader)
+	if err != nil {
+		oggWriter.CloseWithError(err)
+		return nil, err
+	}
 
 	r, w := io.Pipe()
+
 	go func() {
 		encoder := json.NewEncoder(w)
 
-		for result := range at.ResultCh {
-			if err := result.Error; err != nil {
-				w.CloseWithError(err)
+	L:
+		for {
+			select {
+			case <-ctx.Done():
 				return
+			case event := <-stream.Events():
+				switch e := event.(type) {
+				case *transcribestreamingservice.TranscriptEvent:
+					for _, res := range e.Transcript.Results {
+						var result AwsResult
+						result.Type = "aws"
+						if at.Config.AwsResultIsPartial {
+							result.WithIsPartial(*res.IsPartial)
+						}
+						if at.Config.AwsResultChannelID {
+							result.WithChannelID(*res.ChannelId)
+						}
+						for _, alt := range res.Alternatives {
+							var message string
+							if alt.Transcript != nil {
+								message = *alt.Transcript
+							}
+							result.Message = message
+							if err := encoder.Encode(result); err != nil {
+								w.CloseWithError(err)
+								return
+							}
+						}
+					}
+				default:
+					break L
+				}
 			}
+		}
 
-			if err := encoder.Encode(result); err != nil {
-				w.CloseWithError(err)
-				return
-			}
+		if err := stream.Err(); err != nil {
+			err := fmt.Errorf("UNEXPECTED-STREAM-EVENT: %w", err)
+			w.CloseWithError(err)
+			return
 		}
 	}()
 
