9ecba0646a0141aa1d1d717d52b47f61bebed3b2
Yoshida Hiroshi <yoshida@shiguredo.jp>
2023-04-25 16:15:24 +0900

    設定ファイルを toml から ini に変更する

--- config.go
+++ config.go
@@ -1,7 +1,7 @@
 package suzu
 
 import (
-	"github.com/BurntSushi/toml"
+	"gopkg.in/ini.v1"
 )
 
 const (
@@ -12,77 +12,83 @@ const (
 type Config struct {
 	Revision string
 
-	Debug bool `toml:"debug"`
+	Debug bool `ini:"debug"`
 
-	ListenAddr string `toml:"listen_addr"`
-	ListenPort int    `toml:"listen_port"`
+	ListenAddr string `ini:"listen_addr"`
+	ListenPort int    `ini:"listen_port"`
 
-	HTTP2FullchainFile    string `toml:"http2_fullchain_file"`
-	HTTP2PrivkeyFile      string `toml:"http2_privkey_file"`
-	HTTP2VerifyCacertPath string `toml:"http2_verify_cacert_path"` // クライアント認証用
+	HTTP2FullchainFile    string `ini:"http2_fullchain_file"`
+	HTTP2PrivkeyFile      string `ini:"http2_privkey_file"`
+	HTTP2VerifyCacertPath string `ini:"http2_verify_cacert_path"` // クライアント認証用
 
-	HTTP2MaxConcurrentStreams uint32 `toml:"http2_max_concurrent_streams"`
-	HTTP2MaxReadFrameSize     uint32 `toml:"http2_max_read_frame_size"`
-	HTTP2IdleTimeout          uint32 `toml:"http2_idle_timeout"`
+	HTTP2MaxConcurrentStreams uint32 `ini:"http2_max_concurrent_streams"`
+	HTTP2MaxReadFrameSize     uint32 `ini:"http2_max_read_frame_size"`
+	HTTP2IdleTimeout          uint32 `ini:"http2_idle_timeout"`
 
-	Retry *bool `toml:"retry"`
+	Retry *bool `ini:"retry"`
 
-	ExporterIPAddress string `toml:"exporter_ip_address"`
-	ExporterPort      int    `toml:"exporter_port"`
+	ExporterIPAddress string `ini:"exporter_ip_address"`
+	ExporterPort      int    `ini:"exporter_port"`
 
-	SkipBasicAuth     bool   `toml:"skip_basic_auth"`
-	BasicAuthUsername string `toml:"basic_auth_username"`
-	BasicAuthPassword string `toml:"basic_auth_password"`
+	SkipBasicAuth     bool   `ini:"skip_basic_auth"`
+	BasicAuthUsername string `ini:"basic_auth_username"`
+	BasicAuthPassword string `ini:"basic_auth_password"`
 
-	SampleRate   int `toml:"audio_sample_rate"`
-	ChannelCount int `toml:"audio_channel_count"`
+	SampleRate   int `ini:"audio_sample_rate"`
+	ChannelCount int `ini:"audio_channel_count"`
 
-	DumpFile string `toml:"dump_file"`
+	DumpFile string `ini:"dump_file"`
 
-	LogDir    string `toml:"log_dir"`
-	LogName   string `toml:"log_name"`
-	LogDebug  bool   `toml:"log_debug"`
-	LogStdout bool   `toml:"log_stdout"`
+	LogDir    string `ini:"log_dir"`
+	LogName   string `ini:"log_name"`
+	LogDebug  bool   `ini:"log_debug"`
+	LogStdout bool   `ini:"log_stdout"`
 
-	TimeToWaitForOpusPacketMs int `toml:"time_to_wait_for_opus_packet_ms"`
+	TimeToWaitForOpusPacketMs int `ini:"time_to_wait_for_opus_packet_ms"`
 
 	// aws の場合は IsPartial が false, gcp の場合は IsFinal が true の場合にのみ結果を返す指定
-	FinalResultOnly bool `toml:"final_result_only"`
+	FinalResultOnly bool `ini:"final_result_only"`
 
 	// Amazon Web Services
-	AwsCredentialFile                    string `toml:"aws_credential_file"`
-	AwsProfile                           string `toml:"aws_profile"`
-	AwsRegion                            string `toml:"aws_region"`
-	AwsEnablePartialResultsStabilization bool   `toml:"aws_enable_partial_results_stabilization"`
-	AwsPartialResultsStability           string `toml:"aws_partial_results_stability"`
-	AwsEnableChannelIdentification       bool   `toml:"aws_enable_channel_identification"`
+	AwsCredentialFile                    string `ini:"aws_credential_file"`
+	AwsProfile                           string `ini:"aws_profile"`
+	AwsRegion                            string `ini:"aws_region"`
+	AwsEnablePartialResultsStabilization bool   `ini:"aws_enable_partial_results_stabilization"`
+	AwsPartialResultsStability           string `ini:"aws_partial_results_stability"`
+	AwsEnableChannelIdentification       bool   `ini:"aws_enable_channel_identification"`
 	// 変換結果に含める項目の有無の指定
-	AwsResultChannelID bool `toml:"aws_result_channel_id"`
-	AwsResultIsPartial bool `toml:"aws_result_is_partial"`
+	AwsResultChannelID bool `ini:"aws_result_channel_id"`
+	AwsResultIsPartial bool `ini:"aws_result_is_partial"`
 
 	// Google Cloud Platform
-	GcpCredentialFile                      string   `toml:"gcp_credential_file"`
-	GcpEnableSeparateRecognitionPerChannel bool     `toml:"gcp_enable_separate_recognition_per_channel"`
-	GcpAlternativeLanguageCodes            []string `toml:"gcp_alternative_language_codes"`
-	GcpMaxAlternatives                     int32    `toml:"gcp_max_alternatives"`
-	GcpProfanityFilter                     bool     `toml:"gcp_profanity_filter"`
-	GcpEnableWordTimeOffsets               bool     `toml:"gcp_enable_word_time_offsets"`
-	GcpEnableWordConfidence                bool     `toml:"gcp_enable_word_confidence"`
-	GcpEnableAutomaticPunctuation          bool     `toml:"gcp_enable_automatic_punctuation"`
-	GcpEnableSpokenPunctuation             bool     `toml:"gcp_enable_spoken_punctuation"`
-	GcpEnableSpokenEmojis                  bool     `toml:"gcp_enable_spoken_emojis"`
-	GcpModel                               string   `toml:"gcp_model"`
-	GcpUseEnhanced                         bool     `toml:"gcp_use_enhanced"`
-	GcpSingleUtterance                     bool     `toml:"gcp_single_utterance"`
-	GcpInterimResults                      bool     `toml:"gcp_interim_results"`
+	GcpCredentialFile                      string   `ini:"gcp_credential_file"`
+	GcpEnableSeparateRecognitionPerChannel bool     `ini:"gcp_enable_separate_recognition_per_channel"`
+	GcpAlternativeLanguageCodes            []string `ini:"gcp_alternative_language_codes"`
+	GcpMaxAlternatives                     int32    `ini:"gcp_max_alternatives"`
+	GcpProfanityFilter                     bool     `ini:"gcp_profanity_filter"`
+	GcpEnableWordTimeOffsets               bool     `ini:"gcp_enable_word_time_offsets"`
+	GcpEnableWordConfidence                bool     `ini:"gcp_enable_word_confidence"`
+	GcpEnableAutomaticPunctuation          bool     `ini:"gcp_enable_automatic_punctuation"`
+	GcpEnableSpokenPunctuation             bool     `ini:"gcp_enable_spoken_punctuation"`
+	GcpEnableSpokenEmojis                  bool     `ini:"gcp_enable_spoken_emojis"`
+	GcpModel                               string   `ini:"gcp_model"`
+	GcpUseEnhanced                         bool     `ini:"gcp_use_enhanced"`
+	GcpSingleUtterance                     bool     `ini:"gcp_single_utterance"`
+	GcpInterimResults                      bool     `ini:"gcp_interim_results"`
 	// 変換結果に含める項目の有無の指定
-	GcpResultIsFinal   bool `toml:"gcp_result_is_final"`
-	GcpResultStability bool `toml:"gcp_result_stability"`
+	GcpResultIsFinal   bool `ini:"gcp_result_is_final"`
+	GcpResultStability bool `ini:"gcp_result_stability"`
 }
 
 func InitConfig(data []byte, config *Config) error {
-	if err := toml.Unmarshal(data, config); err != nil {
+	f, err := ini.InsensitiveLoad(data)
+	if err != nil {
 		// パースに失敗した場合 Fatal で終了
+		return err
+	}
+
+	if err := f.MapTo(config); err != nil {
+		// マッピングに失敗した場合 Fatal で終了
 		return err
 	}
 
--- go.mod
+++ go.mod
@@ -4,7 +4,6 @@ go 1.20
 
 require (
 	cloud.google.com/go/speech v1.15.0
-	github.com/BurntSushi/toml v1.2.1
 	github.com/aws/aws-sdk-go v1.44.249
 	github.com/labstack/echo-contrib v0.14.1
 	github.com/labstack/echo/v4 v4.10.2
@@ -20,6 +19,7 @@ require (
 	google.golang.org/api v0.119.0
 	google.golang.org/grpc v1.54.0
 	google.golang.org/protobuf v1.30.0
+	gopkg.in/ini.v1 v1.67.0
 )
 
 require (
